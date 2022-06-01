c64e4b9dc990cfb59c5740d4b685fac7da53e9bc
Avinashbbb <avinash.bbb@gmail.com>
2022-06-01 13:18:56 -0400

    Fixed most bugs with the js needs to just fix 1 bug

--- Smarty/HTML_BS4/Quote.js
+++ Smarty/HTML_BS4/Quote.js
@@ -3,17 +3,9 @@ $(document).ready(function(){
 
     $(document).change(()=>{
         selectionOfBuilding();
-        $("#appartments").val();
-        $("#floors").val();
-        $("#basments").val();
-        $("#companies").val();
-        $("#parking-spots").val();
-        $("#elvators").val();
-        $("#corporations").val();
-        $("#occupancy").val();
-        $("#hours").val();
     });
 });
+$("#step_1").on("input",()=>{resetValue()});
 
 //declaring building
 let residential = $("#residential").text();
@@ -41,10 +33,6 @@ var final_price = 0;
 
 //declarations for thr input
 // let appartments = $("#appartments").on("input").value;
-
-
-
-
 //console.log(residential,commercial,corporate,hybrid,select_Type_Of_Building)
 
 //function to select the type of building!!
@@ -98,13 +86,11 @@ function hyb_selected (){
 }
 //restlts 
 function mainCalculation(input){
-    
-    $("#select_service").on ("change", ()=> {
+        
         var radios = document.getElementsByName("type_of_Service");
         var selected = Array.from(radios).find(radio => radio.checked);
         
         if (selected.value == "standard"){
-            console.log(input)
             elevators_needed = input;
             one_elevator = 7565;
             total_evevators = 7565*elevators_needed;
@@ -128,14 +114,37 @@ function mainCalculation(input){
             final_price = installation+total_evevators;
             results();
         }
-    });
+    
 }
+
+
 function results(){
-$("#elevators_needed").html(elevators_needed),
-$("#one_elevator").html(one_elevator),
-$("#total_evevators").html(total_evevators),
-$("#installation").html(installation.toFixed(2)),
-$("#final_price").html(final_price.toFixed(2))
+    let res = [$("#appartments").val(),$("#floors").val(),$("#basments").val()]
+    let com =[$("floors").val(),$("#basments").val(),$("#companies").val(),$("#parking-spots").val(),$("#elvators").val()]
+    let hyd =[$("#floors").val(),$("#basments").val(),$("#companies").val(),$("#parking-spots").val(),$("#occupancy").val(),$("#hours").val()]
+    let cor =[$("#floors").val(),$("#basments").val(),$("#parking-spots").val(),$("#corporations").val(),$("#occupancy").val()]
+    checking();
+    function checking (){
+        res = res.every(element => element)
+        com = com.every(element => element)
+        hyd = hyd.every(element => element)
+        cor = cor.every(element => element)
+        }
+
+    if( res||com||hyd||cor){
+        $("#elevators_needed").html(elevators_needed),
+        $("#one_elevator").html(one_elevator.toLocaleString("en-US", {style:"currency", currency:"USD"})),
+        $("#total_evevators").html(total_evevators.toLocaleString("en-US", {style:"currency", currency:"USD"})),
+        $("#installation").html(installation.toLocaleString("en-US", {style:"currency", currency:"USD"})),
+        $("#final_price").html(final_price.toLocaleString("en-US", {style:"currency", currency:"USD"}))
+    }
+    else{
+        $("#elevators_needed").html("Please"),
+        $("#one_elevator").html("Enter"),
+        $("#total_evevators").html("All The"),
+        $("#installation").html("Needed"),
+        $("#final_price").html("Information")
+    }
 }
 
 
@@ -229,6 +238,19 @@ function showHyb(){
         $("#business-hours").hide();
         $("#line").hide();
         $("#results").hide();
+    }
+
+    function resetValue() {
+        console.log("i am working")
+        $("#appartments").val(" ");
+        $("#floors").val(" ");
+        $("#basments").val(" ");
+        $("#companies").val(" ");
+        $("#parking-spots").val(" ");
+        $("#elvators").val(" ");
+        $("#corporations").val(" ");
+        $("#occupancy").val(" ");
+        $("#hours").val(" ");
     }
 
         //console.log(residential,commercial,corporate,hybrid,select_Type_Of_Building)
